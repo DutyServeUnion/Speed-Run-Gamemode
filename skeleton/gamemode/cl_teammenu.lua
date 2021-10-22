b5d9341e0fb7ecd641729421b7911a2e22ae4738
Andrew <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-22 13:21:19 +0400

    ok

--- skeleton/gamemode/cl_teammenu.lua
+++ skeleton/gamemode/cl_teammenu.lua
@@ -1,8 +1,16 @@
 function TeamMenu()
-
+    local tmenu = vgui.Create("DFrame")
+    tmenu:Center()
+    tmenu:SetSize(200, 150)
+    tmenu:SetTitle("Team Menu")
+    tmenu:MakeDraggable( true ) 
+    tmenu:ShowCloseButton( true ) 
+    tmenu:SetVisible(true)
+    tmenu:MakePopup()
+    tmenu.Paint = function
+        draw.RoundedBox(1,1,1,1000,1000,Color(43, 199, 22))
+    
 end
 
 -- team config
-concommand.Add("team_1",function()
-    end
-end)
+concommand.Add("team_menu, TeamMenu)

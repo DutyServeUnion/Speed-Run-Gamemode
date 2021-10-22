33bac75efdf7118b6cbdb5fa48dbafeeff50d303
Andrew <84892309+DutyServeUnion@users.noreply.github.com>
2021-10-22 13:54:06 +0400

    Update init.lua

--- skeleton/gamemode/init.lua
+++ skeleton/gamemode/init.lua
@@ -15,3 +15,11 @@ end
 hook.Add("ShowHelp, "OpenTeamMenu", function()
     concommand.Run(ply,"team_menu")
 end)
+
+concommand.Add("team_1", function(ply)
+    ply:SetTeam(1)
+end)
+
+concommand.Add("team_2", function(ply)
+    ply:SetTeam(2)
+end)
